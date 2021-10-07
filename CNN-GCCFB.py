b1d7f08951d327409c17dc65113fb450f1876f52
fuyanjie <fyjneverfollows@163.com>
2021-10-07 21:20:20 +0800

    Feature/train_on_frame_level_with_single_source

---
+++ CNN-GCCFB.py
@@ -0,0 +1,92 @@
+import torch.nn as nn
+import torch
+
+from torch import optim
+
+import prepare_data_at_frame_level
+
+
+# design model
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        # first conv and second conv
+        self.conv_1 = nn.Sequential(
+            nn.Conv2d(6, 12, kernel_size=(5, 5), stride=2),
+            nn.BatchNorm2d(12), nn.ReLU(inplace=True)
+        )
+        self.conv_2 = nn.Sequential(
+            nn.Conv2d(12, 24, kernel_size=(5, 5), stride=2),
+            nn.BatchNorm2d(24), nn.ReLU(inplace=True)
+        )
+        self.conv_3 = nn.Sequential(
+            nn.Conv2d(24, 48, kernel_size=(5, 5), stride=2),
+            nn.BatchNorm2d(48), nn.ReLU(inplace=True)
+        )
+        self.conv_4 = nn.Sequential(
+            nn.Conv2d(48, 96, kernel_size=(5, 5), stride=2),
+            nn.BatchNorm2d(96), nn.ReLU(inplace=True)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(in_features=96*18*24, out_features=360),
+            # nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.conv_1(x)
+        print("after conv_1 x.shape:{}".format(x.shape))
+        x = self.conv_2(x)
+        print("after conv_2 x.shape:{}".format(x.shape))
+        x = self.conv_3(x)
+        print("after conv_3 x.shape:{}".format(x.shape))
+        x = self.conv_4(x)
+        print("after conv_4 x.shape:{}".format(x.shape))
+        # flatten data from (n, 96, 18, 24) to (n, 96*18*24)
+        x = x.view(-1, 96*18*24)
+        print('before fc x.shape:{}'.format(x.shape))
+        x = self.fc(x)
+
+        return x
+
+
+model = CNN()
+
+# construct loss and optimizer
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+
+# training cycle forward, backward, update
+def train(epoch):
+    running_loss = 0.0
+
+    data_loader = prepare_data_at_frame_level.DataLoader()
+    # Train
+    iter = 0
+    for (batch_x, batch_y) in data_loader.generate():
+        # 获得一个批次的数据和标签(inputs, labels)
+        print("batch_x {}".format(batch_x.shape))
+        print("batch_y {}".format(batch_y.shape))
+        # 获得模型预测结果
+        output = model(batch_x)
+        # 交叉熵代价函数
+        loss = criterion(output, batch_y)
+
+        running_loss += loss.item()
+        if iter % 100 == 0:
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, iter + 1, running_loss / 100))
+            running_loss = 0.0
+
+        # Backward
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        iter += 1
+
+        # Evaluate
+
+
+if __name__ == '__main__':
+    for epoch in range(10):
+        train(epoch)
---
+++ prepare_data_at_frame_level.py
@@ -0,0 +1,66 @@
+import math
+import os
+import pickle
+import random
+
+import numpy as np
+import torch
+from torch import nn
+
+train_data_path = "/Users/fuyanjie/Desktop/temp/exp_nnsslm/train_data_frame_level"
+
+
+class DataLoader(object):
+    def __init__(self, batchsize, shuffle=False):
+        self.batch_size = batchsize
+        self.shuffle = shuffle
+
+    def generate(self):
+        frames = os.listdir(train_data_path)[:201]
+        cnt_files = len(frames)
+        if self.shuffle:
+            random.shuffle(frames)
+
+        batch_size = self.batch_size
+        start = 0
+        while start < cnt_files:
+            batch_frame = frames[start : min(start + batch_size, cnt_files)]
+
+            batch_x = []
+            batch_y = []
+
+            for frame in batch_frame:
+                origin_data = np.load(os.path.join(train_data_path, frame), allow_pickle=True)
+                # pick out single source labels
+                ########## remove the code below to cancel filtering single source labels #####################
+                if np.isnan(origin_data[1][0]) or not np.isnan(origin_data[1][1]):
+                    continue
+                y = torch.LongTensor([origin_data[1][0]])
+                ###############################################################################################
+                # y = torch.Tensor(origin_data[1])
+                print(y)
+                batch_y.append(y)
+
+                x = torch.Tensor(origin_data[0])
+                batch_x.append(x)
+
+            start += batch_size
+            ########## remove the code below to cancel filtering single source labels #####################
+            if not batch_x:
+                continue
+            ###############################################################################################
+            # batch_x.shape should be torch.Size([4, 6, 40, 51])
+            batch_x = torch.stack(batch_x, dim=0)
+            # print("batch_x.shape {}".format(batch_x.shape))
+            # batch_y.shape should be torch.Size([4, 2])
+            batch_y = torch.stack(batch_y, dim=0)
+            # print("batch_y {}".format(batch_y))
+            print("======")
+
+            yield batch_x, batch_y
+
+if __name__ == '__main__':
+    data_loader = DataLoader(batchsize=4, shuffle=True)
+    for batch_x, batch_y in data_loader.generate():
+        print("batch_x {}".format(batch_x.shape))
+        print("batch_y {}".format(batch_y.shape))
