44d1aa5734f95c234391358948643b079617b234
fuyanjie <fyjneverfollows@163.com>
2021-11-09 10:35:06 +0800

    add qsub script

--- scripts/qsub_feat_ex.sh
+++ scripts/qsub_feat_ex.sh
@@ -11,7 +11,7 @@
 #$ -j y
 
 # resource requesting, e.g. for gpu use
-#$ -l h=gpu06
+#$ -l h=gpu03
 
 # remember to activate your conda env
 source activate nnsslm_env
@@ -20,10 +20,12 @@ echo "job start time: `date`"
 # start whatever your job below, e.g., python, matlab, etc.
 #ADD YOUR COMMAND HERE,LIKE python3 main.py
 
-CUDA_VISIBLE_DEVICES=2,3 \
-./apply_feature_extraction.sh ../extract_gcc_fbank.py /Work20/2021/fuyanjie/pycode/nnsslm/train_data_dir/lsp_train_301 gcc_fbank
+# ../scripts/apply_feature_extraction.sh ../extract_gcc_fbank.py /Work18/2021/fuyanjie/exp_data/exp_nnsslm/test_data_dir/lsp_test_library gcc_fbank "-w 8192 -o 4096"
+# ../scripts/apply_feature_extraction.sh ../extract_gcc_fbank.py /Work18/2021/fuyanjie/exp_data/exp_nnsslm/test_data_dir/lsp_test_106 gcc_fbank "-w 8192 -o 4096"
+# ../scripts/apply_feature_extraction.sh ../extract_gcc_fbank.py /Work18/2021/fuyanjie/exp_data/exp_nnsslm/train_data_dir/lsp_train_106 gcc_fbank "-w 8192 -o 4096"
+../scripts/apply_feature_extraction.sh ../extract_gcc_fbank.py /Work18/2021/fuyanjie/exp_data/exp_nnsslm/train_data_dir/lsp_train_301 gcc_fbank "-w 8192 -o 4096"
 
-
+# python ../extract_gcc_fbank.py /Work20/2021/fuyanjie/exp_data/exp_nnsslm/train_data_dir/lsp_train_301/audio/ssl-data_2017-05-06-14-11-12_0.wav /Work20/2021/fuyanjie/exp_data/exp_nnsslm/train_data_dir/lsp_train_301/features/gcc_fbank -w 8192 -o 4096
 #chmod a+x run.sh
 
 # hostname
